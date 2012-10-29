5cdbb78b4954f6cc52b00ddf0417903df06c0f8c
Gordon Marler <gmarler@gmarler.com>
2012-10-29 15:58:30 -0400

    Beginnings of re-reading dir

--- C/ftruncate/main.c
+++ C/ftruncate/main.c
@@ -1,15 +1,25 @@
 
 #include "tempfile.h"
+#include <dirent.h>
 
 int main(void)
 {
   char *mydir = "./TDIR";
   int i;
   size_t fsize;
+  DIR *dir;
+  struct dirent *ent;
 
-  for (i = 0; i < 75000; i++) {
+  for (i = 0; i < 750; i++) {
     fsize = rand_tempfile_size();
     create_tempfile(mydir,fsize);
+  }
+
+  dir = opendir(mydir);
+  if (dir != NULL) {
+    while((ent = readdir(dir)) != NULL) {
+      filename = ent->d_name;
+    }
   }
 
 }
