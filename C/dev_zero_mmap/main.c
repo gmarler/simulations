67cb6fd191e54e59aeb079199e27000231111354
Gordon Marler <gmarler@gmarler.com>
2013-03-12 21:59:27 -0400

    Initial test - mmap dev zero

---
+++ C/dev_zero_mmap/main.c
@@ -0,0 +1,164 @@
+
+#include "tempfile.h"
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <getopt.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+  char *mydir;
+  char *temp_fname[5000];
+  char *central_buffer, *bufptr;
+  int i;
+  size_t fsize;
+  DIR *dir;
+  struct dirent *ent;
+  struct stat stat_buf;
+  char path[1024];
+  int fd, dev_zero_fd, ret, filecount;
+  int c;
+  struct timespec ns;
+  ssize_t bufsize = 63 * 1024 * 1024;
+
+  ns.tv_sec = 0;
+  ns.tv_nsec = 250000000;
+
+  /* Flag set by `--verbose'. */
+  static int verbose_flag;
+
+  /*  Initialize temporary file list so we know when it ends */
+  for (i = 0; i < 5000; i++) {
+    temp_fname[i] = NULL;
+  }
+
+  while (1) {
+    static struct option long_options[] = {
+      /* These options set a flag. */
+      {"verbose",   no_argument, &verbose_flag, 1},
+      {"brief",     no_argument, &verbose_flag, 0},
+      /* The following options don't set a flag. */
+      {"bufsize",   required_argument, NULL, 'b'},
+      {"count",     required_argument, NULL, 'c'},
+      {"dirname",   required_argument, NULL, 'd'},
+      {0, 0, 0, 0}
+    };
+    /* getopt_long stores the option index here. */
+    int option_index = 0;
+
+    c = getopt_long (argc, argv, "b:c:d:",
+        long_options, &option_index);
+
+    /* Detect the end of the options. */
+    if (c == -1)
+      break;
+
+    switch (c) {
+      case 0:
+        /* (In this example) only options which set */
+        /* a flag return zero, so do nothing. */
+        break;
+
+      case 'b':
+        printf ("option --bufsize (-b) with value `%s'\n", optarg);
+        bufsize = atoi(optarg);
+        break;
+
+      case 'c':
+        printf ("option --count (-c) with value `%s'\n", optarg);
+        filecount = atoi(optarg);
+        break;
+
+      case 'd':
+        printf ("option --dirname (-d) with value `%s'\n", optarg);
+        mydir = optarg;
+        break;
+
+      case '?':
+        /* getopt_long already printed an error message. */
+        break;
+
+      default:
+        abort ();
+    }
+  }
+
+  /* Instead of reporting `--verbose'
+     and `--brief' as they are encountered,
+     we report the final status resulting from them. */
+  if (verbose_flag)
+    puts ("verbose flag is set");
+
+  /* Print any remaining command line arguments (not options). */
+  if (optind < argc) {
+    printf ("non-option ARGV-elements: ");
+    while (optind < argc)
+      printf ("%s ", argv[optind++]);
+    putchar ('\n');
+  }
+
+  for (i = 0; i < filecount; i++) {
+    fsize = rand_tempfile_size();
+    temp_fname[i] = create_tempfile(mydir,fsize);
+  }
+
+  if ((dev_zero_fd = open("/dev/zero", O_RDWR)) == -1) {
+    perror("Unable to open /dev/zero");
+    exit(1);
+  }
+
+  if ((central_buffer = memalign((size_t)8192, bufsize)) == NULL) {
+    perror("Unable to allocate central buffer");
+    exit(1);
+  }
+
+  dir = opendir(mydir);
+  if (dir != NULL) {
+    /* skip '.' and '..' */
+    if ((readdir(dir) != NULL) && (readdir(dir) != NULL)) {
+      while((ent = readdir(dir)) != NULL) {
+        snprintf(path, 1024, "%s/%s", mydir, ent->d_name);
+        if (lstat(path, &stat_buf) == -1) {
+          perror("lstat failed");
+          continue;
+        }
+        switch (stat_buf.st_mode & S_IFMT) {
+          case S_IFREG:
+            /* Test: mmap /dev/zero */
+            if ((bufptr = mmap(central_buffer, bufsize, PROT_READ | PROT_WRITE,
+                               MAP_SHARED | MAP_FIXED, dev_zero_fd,
+                               0)) == MAP_FAILED) {
+              perror("Unable to mmap /dev/zero");
+              continue;
+            }
+            /* Test: mmap file */
+            /*
+            if ((fd = open(path,O_RDWR)) == -1) {
+              perror("Unable to open file for mmap\n");
+              continue;
+            }
+
+            close(fd); */
+            /* 4 per sec (Hz) */
+            /*  nanosleep(&ns,(struct timespec *)NULL); */
+            break;
+
+          default:
+            continue;
+        }
+      }
+    }
+  }
+
+  for (i = 0; i < 5000; i++) {
+    if (temp_fname[i] == NULL) break;
+    printf("Unlinking %s\n",temp_fname[i]);
+    unlink(temp_fname[i]);
+    free(temp_fname[i]);  /* was allocated when temp file created, so free here */
+  }
+}
---
+++ C/dev_zero_mmap/tempfile.c
@@ -0,0 +1,125 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  tempfile.c
+ *
+ *    Description:  Library of routines for temporary file manipulation
+ *
+ *        Version:  1.0
+ *        Created:  10/29/12 11:41:12 AM
+ *       Revision:  none
+ *       Compiler:  cc (Studio 12)
+ *
+ *         Author:  Gordon Marler
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include "tempfile.h"
+
+char *
+create_tempfile(char *dirpath, size_t size)
+{
+  char    data_buf[65536];
+  char    fname_buf[512];
+  char   *fname_ret; /* Temp filename we return */
+  ssize_t nread,
+          bytes_to_copy,
+          to_read;
+  int     rand_fd,
+          temp_fd;
+  int     i, saved_errno;
+
+  if ((rand_fd = open("/dev/zero",O_RDONLY)) == -1 ) {
+    perror("Unable to open /dev/zero");
+    return NULL;
+  }
+
+  /* prime the string before strcat'ing to it */
+  strncpy(fname_buf,dirpath,512);
+  strcat(fname_buf,"/");
+  strcat(fname_buf,"tempXXXXXX");
+  /* strncpy(fname_buf,strcat(dirpath,strcat("/","tempXXXXXX")),512); */
+  /*  printf("Going to create %s\n",fname_buf); */
+
+  if ((temp_fd = mkstemp(fname_buf)) == -1) {
+    perror("mkstemp");
+    return NULL;
+  }
+
+  bytes_to_copy = size;
+
+  while (bytes_to_copy > 0) {
+    if (bytes_to_copy >= sizeof(data_buf)) {
+      to_read = sizeof(data_buf);
+    } else { to_read = bytes_to_copy; }
+
+    nread = read(rand_fd, data_buf, to_read);
+    char *out_ptr = data_buf;
+    ssize_t         nwritten;
+
+    do {
+      nwritten = write(temp_fd, out_ptr, nread);
+      if (nwritten >= 0) {
+        nread -= nwritten;
+        out_ptr += nwritten;
+        bytes_to_copy -= nwritten;
+      } else if (errno != EINTR) {
+        goto out_error;
+      }
+    } while (nread > 0);
+  }
+
+  if (nread == 0) {
+    if (close(temp_fd) < 0) {
+      temp_fd = -1;
+      goto out_error;
+    }
+    close(rand_fd);
+
+    /* success! */
+    fname_ret = malloc(strlen(fname_buf));
+    strncpy(fname_ret,fname_buf,strlen(fname_buf));
+    return fname_ret;
+  }
+
+out_error:
+  saved_errno = errno;
+  close(rand_fd);
+  if (temp_fd >= 0)
+    close(temp_fd);
+
+  errno = saved_errno;
+  perror("create_tempfile");
+  return NULL;
+}
+
+size_t
+rand_tempfile_size(void)
+{
+  /* Generate random file sizes starting at a minimum of 256 KB */
+  size_t fsizes[12] = {
+    256 * 1024,
+    512 * 1024,
+    768 * 1024,
+    1024 * 1024,
+    1536 * 1024,
+    2048 * 1024,
+    2560 * 1024,
+    3072 * 1024,
+    4096 * 1024,
+    4608 * 1024,
+    5120 * 1024,
+    5632 * 1024
+  };
+  static int initialized = 0;
+
+  if (initialized == 0) {
+    srand(time(NULL));
+    printf("Initializing random number generator\n");
+    initialized++;
+  }
+
+  return(fsizes[rand() % 12]);
+}
